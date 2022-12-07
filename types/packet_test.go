90bb1fdf1e1c21f81c7f0d3501cb82d80f786078
Dreamer <745124335@qq.com>
2022-12-07 15:01:21 +0800

    refactor proto

--- types/packet_test.go
+++ types/packet_test.go
@@ -10,32 +10,32 @@ func TestNonFungibleTokenPacketData_ValidateBasic(t *testing.T) {
 	}{
 		{
 			name:    "valid packet",
-			packet:  NonFungibleTokenPacketData{"cryptoCat", "uri", []string{"kitty"}, []string{"kitty_uri"}, sender, receiver, [][]byte{nil}},
+			packet:  NonFungibleTokenPacketData{"cryptoCat", "uri", []string{"kitty"}, []string{"kitty_uri"}, [][]byte{nil}, sender, receiver},
 			wantErr: false,
 		},
 		{
 			name:    "invalid packet with empty classID",
-			packet:  NonFungibleTokenPacketData{"", "uri", []string{"kitty"}, []string{"kitty_uri"}, sender, receiver, [][]byte{nil}},
+			packet:  NonFungibleTokenPacketData{"", "uri", []string{"kitty"}, []string{"kitty_uri"}, [][]byte{nil}, sender, receiver},
 			wantErr: true,
 		},
 		{
 			name:    "invalid packet with empty tokenIds",
-			packet:  NonFungibleTokenPacketData{"cryptoCat", "uri", []string{}, []string{"kitty_uri"}, sender, receiver, [][]byte{nil}},
+			packet:  NonFungibleTokenPacketData{"cryptoCat", "uri", []string{}, []string{"kitty_uri"}, [][]byte{nil}, sender, receiver},
 			wantErr: true,
 		},
 		{
 			name:    "invalid packet with empty tokenUris",
-			packet:  NonFungibleTokenPacketData{"cryptoCat", "uri", []string{"kitty"}, []string{}, sender, receiver, [][]byte{nil}},
+			packet:  NonFungibleTokenPacketData{"cryptoCat", "uri", []string{"kitty"}, []string{}, [][]byte{nil}, sender, receiver},
 			wantErr: true,
 		},
 		{
 			name:    "invalid packet with empty sender",
-			packet:  NonFungibleTokenPacketData{"cryptoCat", "uri", []string{"kitty"}, []string{}, "", receiver, [][]byte{nil}},
+			packet:  NonFungibleTokenPacketData{"cryptoCat", "uri", []string{"kitty"}, []string{}, [][]byte{nil}, "", receiver},
 			wantErr: true,
 		},
 		{
 			name:    "invalid packet with empty receiver",
-			packet:  NonFungibleTokenPacketData{"cryptoCat", "uri", []string{"kitty"}, []string{}, sender, receiver, [][]byte{nil}},
+			packet:  NonFungibleTokenPacketData{"cryptoCat", "uri", []string{"kitty"}, []string{}, [][]byte{nil}, sender, receiver},
 			wantErr: true,
 		},
 	}
