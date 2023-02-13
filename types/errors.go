4b683b85ccc042284dcec7dc610a22de7bf77929
Dreamer <745124335@qq.com>
2023-02-13 12:38:21 +0800

    fix golangci-lint

--- types/errors.go
+++ types/errors.go
@@ -1,17 +1,17 @@
 package types
 
 import (
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	errorsmod "cosmossdk.io/errors"
 )
 
 // IBC transfer sentinel errors
 var (
-	ErrInvalidPacketTimeout = sdkerrors.Register(ModuleName, 2, "invalid packet timeout")
-	ErrInvalidVersion       = sdkerrors.Register(ModuleName, 3, "invalid ICS721 version")
-	ErrMaxTransferChannels  = sdkerrors.Register(ModuleName, 4, "max nft-transfer channels")
-	ErrInvalidClassID       = sdkerrors.Register(ModuleName, 5, "invalid class id")
-	ErrInvalidTokenID       = sdkerrors.Register(ModuleName, 6, "invalid token id")
-	ErrInvalidPacket        = sdkerrors.Register(ModuleName, 7, "invalid non-fungible token packet")
-	ErrTraceNotFound        = sdkerrors.Register(ModuleName, 8, "classTrace trace not found")
-	ErrMarshal              = sdkerrors.Register(ModuleName, 9, "failed to marshal token data")
+	ErrInvalidPacketTimeout = errorsmod.Register(ModuleName, 2, "invalid packet timeout")
+	ErrInvalidVersion       = errorsmod.Register(ModuleName, 3, "invalid ICS721 version")
+	ErrMaxTransferChannels  = errorsmod.Register(ModuleName, 4, "max nft-transfer channels")
+	ErrInvalidClassID       = errorsmod.Register(ModuleName, 5, "invalid class id")
+	ErrInvalidTokenID       = errorsmod.Register(ModuleName, 6, "invalid token id")
+	ErrInvalidPacket        = errorsmod.Register(ModuleName, 7, "invalid non-fungible token packet")
+	ErrTraceNotFound        = errorsmod.Register(ModuleName, 8, "classTrace trace not found")
+	ErrMarshal              = errorsmod.Register(ModuleName, 9, "failed to marshal token data")
 )
