e7940d9c93791ac159f075618a9112e9fa0cc679
Dreamer <dreamer.x.cn@gmail.com>
2024-06-04 10:33:49 +0800

    fix lint

--- types/codec.go
+++ types/codec.go
@@ -30,8 +30,6 @@ func RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 }
 
 var (
-	amino = codec.NewLegacyAmino()
-
 	// ModuleCdc references the global nft-transfer module codec. Note, the codec
 	// should ONLY be used in certain instances of tests and for JSON encoding.
 	//
@@ -40,14 +38,25 @@ var (
 	ModuleCdc = codec.NewProtoCodec(codectypes.NewInterfaceRegistry())
 
 	// AminoCdc is a amino codec created to support amino json compatible msgs.
-	AminoCdc = codec.NewAminoCodec(amino)
+	AminoCdc = codec.NewLegacyAmino()
 )
 
 func init() {
-	RegisterLegacyAminoCodec(amino)
-	amino.Seal()
+	RegisterLegacyAminoCodec(AminoCdc)
+	AminoCdc.Seal()
 }
 
+// MustProtoMarshalJSON marshals a protobuf message to JSON and panics if there is an error.
+//
+// It takes a protobuf message as input and returns the JSON-encoded byte array.
+// The function uses the provided anyResolver to resolve any protobuf Any types in the message.
+// If there is an error during the marshaling process, the function panics.
+//
+// Parameters:
+// - msg: The protobuf message to be marshaled.
+//
+// Returns:
+// - []byte: The JSON-encoded byte array.
 func MustProtoMarshalJSON(msg proto.Message) []byte {
 	anyResolver := codectypes.NewInterfaceRegistry()
 	bz, err := ProtoMarshalJSON(msg, anyResolver)
